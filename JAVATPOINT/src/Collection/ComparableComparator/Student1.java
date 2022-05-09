7ef8a8d13e0e0c506099ca17e54d2c26aa4bcf8e
Tejas <jawaletejas1299@gmail.com>
2022-05-09 09:28:05 +0530

    Programs

---
+++ JAVATPOINT/src/Collection/ComparableComparator/Student1.java
@@ -0,0 +1,13 @@
+package Collection.ComparableComparator;
+
+class Student1 {
+	int rollno;
+	String name;
+	int age;
+
+	Student1(int rollno, String name, int age) {
+		this.rollno = rollno;
+		this.name = name;
+		this.age = age;
+	}
+}
